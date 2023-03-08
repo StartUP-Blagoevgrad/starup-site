1963efa3d36e008473e8a1371e367a12753f3c88
Rumen Balashov <rumenbalashov@gmail.com>
2023-03-08 17:41:23 +0200

    Add department components TypeScript.

---
+++ src/app/components/departments/Marketing/marketing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marketing',
+  templateUrl: './marketing.component.html',
+  styleUrls: ['./marketing.component.scss']
+})
+export class MarketingComponent {
+  // any logic or data can be added here
+}
