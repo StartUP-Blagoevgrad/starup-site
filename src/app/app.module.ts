76c4df2e330eb7ddaabed339abbe2686f411be30
Rumen Balashov <rumenbalashov@gmail.com>
2023-03-08 17:17:37 +0200

    Add department component TypeScript.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,12 @@ import { TeamComponent } from './pages/team-page/team.component';
 import { EventsPageComponent } from './pages/events-page/events-page.component';
 import { BoardComponent } from './components/departments/board/board.component';
 
+import { BoardComponent } from './components/departments/board/board.component';
+import { ItComponent } from './components/departments/it/it.component';
+import { MarketingComponent } from './components/departments/marketing/marketing.component';
+import { SpeakersComponent } from './components/departments/speakers/speakers.component';
+//import { SponsorshipComponent } from './components/departments/Sponsorship/';
+
 @NgModule({
   declarations: [
     AppComponent, NavigationMenuComponent, TeamComponent, EventsPageComponent, BoardComponent
