9274fcacc21e787e381abaf79f917cfc698fc9ba
Rumen Balashov <rumenbalashov@gmail.com>
2023-03-12 19:15:40 +0200

    Add sponsorship.component.ts and add it to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { NavigationMenuComponent } from './shared/components/navigation-menu/nav
 import { AppComponent } from './app.component';
 import { TeamComponent } from './pages/team-page/team.component';
 import { EventsPageComponent } from './pages/events-page/events-page.component';
-import { BoardComponent } from './components/departments/board/board.component';
 
 import { BoardComponent } from './components/departments/board/board.component';
 import { ItComponent } from './components/departments/it/it.component';
 import { MarketingComponent } from './components/departments/marketing/marketing.component';
 import { SpeakersComponent } from './components/departments/speakers/speakers.component';
-//import { SponsorshipComponent } from './components/departments/Sponsorship/';
+import { SponsorshipComponent } from './components/departments/Sponsorship/sponsorship.component';
 
 @NgModule({
   declarations: [
---
+++ src/app/components/departments/Sponsorship/sponsorship.component.ts
@@ -0,0 +1,11 @@
+import { NgClass } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-sponsorship',
+  templateUrl: './sponsorship.component.html',
+  //styleUrls: ['./sponsorship.component.scss']
+})
+export class SponsorshipComponent {
+  // any logic or data can be added here
+}
