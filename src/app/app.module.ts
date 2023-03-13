66cb0e0b8bd0949290e925a75fd9556f99d374b8
Bozhidar Mindov <bbm210@aubg.edu>
2023-03-13 23:32:12 +0200

    Integrate the team component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,22 @@ import { AppComponent } from './app.component';
 import { DepartmentService } from './shared/services/department.service';
 import { TeamComponent } from './pages/team-page/team.component';
 import { EventsPageComponent } from './pages/events-page/events-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
-import { BoardComponent } from './components/departments/board/board.component';
-import { ItComponent } from './components/departments/it/it.component';
-import { MarketingComponent } from './components/departments/marketing/marketing.component';
-import { SpeakersComponent } from './components/departments/speakers/speakers.component';
-import { SponsorshipComponent } from './components/departments/Sponsorship/sponsorship.component';
+// import { BoardComponent } from './components/departments/board/board.component';
+// import { ItComponent } from './components/departments/it/it.component';
+// import { MarketingComponent } from './components/departments/marketing/marketing.component';
+// import { SpeakersComponent } from './components/departments/speakers/speakers.component';
+// import { SponsorshipComponent } from './components/departments/Sponsorship/sponsorship.component';
 
 @NgModule({
   declarations: [
-    AppComponent, NavigationMenuComponent, TeamComponent, EventsPageComponent, BoardComponent, ItComponent, MarketingComponent, SpeakersComponent, SponsorshipComponent
+    AppComponent, NavigationMenuComponent, TeamComponent, EventsPageComponent, 
+    //BoardComponent, ItComponent, MarketingComponent, SpeakersComponent, SponsorshipComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule, HttpClientModule
   ],
   providers: [DepartmentService],
   bootstrap: [AppComponent]
